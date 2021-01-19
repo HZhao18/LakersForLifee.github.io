ad65b99bb5cb0f3ce2a542955b16c6a671151253
LakersForLifee <77083194+LakersForLifee@users.noreply.github.com>
2021-01-19 18:40:35 -0500

    Add files via upload

---
+++ assignment7.js
@@ -0,0 +1,16 @@
+ 
+
+const JohnWeight = 85
+const JohnHeight = 1.76
+JohnBMI = JohnWeight / JohnHeight ** 2
+
+console.log("John BMI:", JohnBMI)
+
+const LucasWeight = 95
+const LucasHeight = 1.88
+LucasBMI = LucasWeight / LucasHeight **2
+console.log("Lucas BMI:", LucasBMI)
+
+
+console.log("Lucas and John are friends, Lucas is", LucasHeight, "m tall and John is",
+JohnHeight,"m tall. So John has higher BMI than Lucas, that is",JohnBMI>LucasBMI)
